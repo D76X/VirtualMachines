2e06ae8f93432acb7338a6a85f9e808f641da44c
Davide Spano <Davide.Spano@wuerth-phoenix.com>
2022-11-09 17:21:20 +0100

    vm.ps1 edits

--- VirtualMachines.ps1
+++ VirtualMachines.ps1
@@ -37,3 +37,22 @@ Get-ChildItem "Azure:/${subName}/VirtualMachines/${vmName}" | Stop-AzVM -Force -
 # Remove a VM - this stops, deallocates and remove the VM from the subscription
 Remove-AzVM -ResourceGroupName $rg -Name $vmName -AsJob
 Get-ChildItem "Azure:/${subName}/VirtualMachines/${vmName}" | Remove-AzVM -Force -AsJob
+
+# To Generalize a Windows VM we use sysprep
+# sysprep resets the Security Identifiers and other user-based information of the OS
+# OOBE = Out Of Box Experience
+# the /shutdown switch causes the OS to hutdown BUT it does not shutdown and/or deallocate the VM in Azure
+# this must be done manually!
+# https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/sysprep--generalize--a-windows-installation?view=windows-11
+# sysprep.exe /generalize /shutdown /oobe
+
+# The process of Generilization of a Linux VM is labeled Deprovisioning
+# The tools used to Deprovision a Linux Machine in Azure is the Azure Linux Agent [waagent]
+# the concept and purpose is the same as sysprep
+# If the OS of an Linux Azure VM is from the Azure Marketplace then waagent is already installed on them
+# If the OS of an Linux Azure VM is NOT from the Azure Marketplace then waagent must be installed on it
+# from a distribution package or from GitHub
+# The same as before applies in regard to shutting down and deallocating the VM in Azure as waagent shutdowns
+# only the OS
+# sudo waagent -deprovision+user -force
+
